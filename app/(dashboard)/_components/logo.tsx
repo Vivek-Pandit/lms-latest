6684e1ef731139804ae26de9cda5283831cd0fe9
Vivek-Pandit <vivek.pandit.31.01@gmail.com>
2023-12-08 23:15:34 +0530

    Logo update

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -6,12 +6,18 @@ export const Logo = () => {
     <div style={{
       display: "flex",
       justifyContent: "center",
+      
+
     }}>
     <Image
       height={50}
       width={50}
       alt="logo"
       src="/logo.svg"
+      style={{
+        objectFit: "cover",
+        borderRadius: "100px",
+      }}
     />
     <h2 className="text-[#f51515] mt-4 font-extrabold font-size: 2.5em;">EduTech LMS</h2>
     </div>
